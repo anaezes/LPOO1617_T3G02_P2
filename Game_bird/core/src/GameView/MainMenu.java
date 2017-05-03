dee293a7b94e057bd811b732a9a4829457b3bfd6
Unknown <ribe.cristiana@gmail.com>
2017-05-03 14:45:06 +0100

    Options/ScoresMenu prototype

--- Game_bird/core/src/GameView/MainMenu.java
+++ Game_bird/core/src/GameView/MainMenu.java
@@ -137,6 +137,7 @@ public class MainMenu implements Screen {
     }
 
     public void onClickOptions() {
+        game.setScreen(OptionsMenu.GetInstance());
             System.out.println("Options");
     }
 
@@ -145,6 +146,7 @@ public class MainMenu implements Screen {
             System.out.println("Instr");
     }
     public void onClickScores() {
+            game.setScreen(ScoresMenu.GetInstance());
             System.out.println("Scores");
     }
     public void onClickExit() {
---
+++ Game_bird/core/src/GameView/OptionsMenu.java
@@ -0,0 +1,117 @@
+package GameView;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by cristiana on 03-05-2017.
+ */
+
+public class OptionsMenu implements Screen{
+    private Viewport gamePort;
+    private Stage stage;
+    private Texture backGround, btnreturn;
+    private ImageButton goBack;
+    private FlyChicken game;
+
+    private static OptionsMenu instance = null;
+
+    public static OptionsMenu GetInstance() {
+        if(instance == null) {
+            instance = new OptionsMenu(FlyChicken.GetInstance());
+        }
+        return instance;
+    }
+
+    public OptionsMenu(FlyChicken game) {
+        this.game=game;
+        gamePort = new FitViewport(FlyChicken.WIDTH, FlyChicken.HEIGHT, new OrthographicCamera());
+        stage = new Stage(gamePort, game.batch);
+
+
+        backGround = new Texture(Gdx.files.internal("backg.png"));
+        btnreturn = new Texture(Gdx.files.internal("returnbtn.png"));
+        TextureRegion returnBtnRegion = new TextureRegion(btnreturn);
+        TextureRegionDrawable returnBtnDraw = new TextureRegionDrawable(returnBtnRegion);
+        goBack = new ImageButton(returnBtnDraw);
+
+        stage.setDebugParentUnderMouse(true);
+        goBack.setPosition(50,50);
+        stage.addActor(goBack);
+
+
+        goBack.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                if(goBack.isPressed()){
+                    onClickBack();
+                }
+                return true;
+            }
+        });
+    }
+
+    public void onClickBack() {
+        System.out.println("GoBack");
+        game.setScreen(MainMenu.GetInstance());
+
+        System.out.println("GoBack to main menu");
+
+    }
+
+
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.getBatch().begin();
+        stage.getBatch().draw(backGround, 0,0, FlyChicken.WIDTH, FlyChicken.HEIGHT);
+        stage.getBatch().end();
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+
+    }
+
+    @Override
+    public void dispose() {
+        backGround.dispose();
+        stage.dispose();
+    }
+}
