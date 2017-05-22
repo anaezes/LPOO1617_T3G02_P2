72f1db6876a3686d33918c173ec9c4be4d81039d
anaezes <anaezes@gmail.com>
2017-05-22 22:26:10 +0100

    Bird: update level textures

--- Game_bird/core/src/GameLogic/BirdLevelThree.java
+++ Game_bird/core/src/GameLogic/BirdLevelThree.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.math.Circle;
 public class BirdLevelThree extends Bird {
     public BirdLevelThree(int x, int y) {
         super(x, y);
-        birdTexture = new Texture("birdanimation2.png");
+        birdTexture = new Texture("birdAnimationHot.png");
         weight = 11f;
         birdAnimation = new Animation(new TextureRegion(birdTexture), 3, 0.5f);
         bounds = new Circle(x+birdAnimation.getFrame().getRegionWidth()/2, y+birdAnimation.getFrame().getRegionHeight()/2, birdAnimation.getFrame().getRegionHeight()/4);
--- Game_bird/core/src/GameLogic/BirdLevelTwo.java
+++ Game_bird/core/src/GameLogic/BirdLevelTwo.java
@@ -9,7 +9,7 @@ public class BirdLevelTwo extends Bird {
 
     public BirdLevelTwo(int x, int y) {
         super(x, y);
-        birdTexture = new Texture("birdanimation2.png");
+        birdTexture = new Texture("birdAnimationWinter.png");
         weight = 5f;
         birdAnimation = new Animation(new TextureRegion(birdTexture), 3, 0.5f);
         bounds = new Circle(x+birdAnimation.getFrame().getRegionWidth()/2, y+birdAnimation.getFrame().getRegionHeight()/2, birdAnimation.getFrame().getRegionHeight()/4);
