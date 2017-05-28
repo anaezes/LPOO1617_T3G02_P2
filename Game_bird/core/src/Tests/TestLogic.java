0698bb6a33d7fb9abf8d11ebd9d2c55f3e78cff5
anaezes <anaezes@gmail.com>
2017-05-28 16:03:41 +0100

    Init tests

--- Game_bird/core/src/Tests/TestLogic.java
+++ Game_bird/core/src/Tests/TestLogic.java
@@ -1,8 +1,21 @@
 package Tests;
 
-/**
- * Created by cristiana on 30-04-2017.
- */
+import org.junit.Assert;
+import org.junit.Test;
+
+import GameLogic.EnumGameLevel;
+import GameLogic.GameMain;
 
 public class TestLogic {
+     @Test
+    public void testBirdEatingApple() {
+         GameMain game = new GameMain(EnumGameLevel.LevelOne);
+         game.createBird(200, 100, 100);
+         game.getEatenApples();
+         Assert.assertEquals(0, game.getEatenApples());
+         game.createApple(100, 120, 100, 100);
+         game.getGameBird().jump();
+         Assert.assertNotEquals(0, game.getEatenApples());
+         Assert.assertEquals(1, game.getEatenApples());
+     }
 }
